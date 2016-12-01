edecf16f138c38c54f1346c4aa54af62fe0c9d73
Phillip Markert <phillip@ephisys.com>
2016-12-01 00:24:01 -0800

    Refactored out alexa-skill-tester module to a new package

--- test/index.js
+++ test/index.js
@@ -1,50 +1,19 @@
-global.TEST_MODE = true;
 process.env.APP_ID = "amzn1.ask.skill.9200600c-ea41-4400-adbe-beb6efef00b1";
-const fs = require("fs");
-const context = require('aws-lambda-mock-context');
-const index = require("../index");
-const async = require("async");
-const chai = require("chai").should();
-const path = require("path");
+var ast = require("alexa-skill-tester");
+var path = require("path");
+var module_under_test = require("../index");
 
-describe("Event processing:", function(done) {
-	async.each(fs.readdirSync("./test/events"), function (dirname, dir_cb) {
-		if(fs.lstatSync(path.join("./test/events", dirname)).isDirectory()) {
-			describe("State:" + dirname, function() {
-				async.each(fs.readdirSync(path.resolve(__dirname, path.join("events", dirname))), function(filename) {
-					if(!filename.endsWith(".response.json")) {
-						it("Case:" + filename, function (event_done) {
-							const file_content = require("./" + path.join("events", dirname, filename));
-							const ctx = context();
-							index.handler(file_content, ctx);
-							ctx.Promise
-								.then(response => {
-									try {
-										const response_filename = path.join("events", dirname, filename.substring(0,filename.length - 5) + ".response.json");
-										if(!fs.existsSync(path.resolve(__dirname, response_filename))) {
-											if(process.env.SAVE_RESPONSES) {
-												fs.writeFileSync(path.resolve(__dirname, response_filename), JSON.stringify(response, null, 2));
-												console.log(`Response for ${response_filename} saved.`);
-											} else {
-												console.log(`Response for ${response_filename} does not exist:`);
-												console.log(JSON.stringify(response, null, 2));
-											}
-											return event_done(`Response file ${response_filename} does not exist yet.`);
-										}
-										const expected_response = require("./" + response_filename);
-										response.should.deep.equal(expected_response);			
-										return event_done();
-									}
-									catch(err) {
-										return event_done(err);
-									}
-								})
-								.catch(event_done);
-						});
-					}
-				}, dir_cb);
-			});
-		}
-		else { dir_cb(); }
-	}, done);
+describe("Event tests", function(done) {
+	describe("search", function(cb) {
+		ast(module_under_test.handler, path.resolve(__dirname, path.join("events", "search")), cb);
+	});
+	describe("list", function(cb) {
+		ast(module_under_test.handler, path.resolve(__dirname, path.join("events", "list")), cb);
+	});
+	describe("none", function(cb) {
+		ast(module_under_test.handler, path.resolve(__dirname, path.join("events", "none")), cb);
+	});
+	describe("list_summary", function(cb) {
+		ast(module_under_test.handler, path.resolve(__dirname, path.join("events", "list_summary")), cb);
+	});
 });
